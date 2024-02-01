e9d6852dd6fb147d830f8385df8f053abe0c9d78
litpu <litput0@gmail.com>
2024-02-01 22:24:26 +0300

    Вариант 2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     public string CyrillicPhrase;
     public string LatinPhrase;
     public string GeneratedPassword;
-    
+
     public InformationAboutPassword(string cyrillicPhrase, string latinPhrase, string generatedPassword)
     {
         CyrillicPhrase = cyrillicPhrase;
@@ -13,16 +13,18 @@
 
     public override string ToString()
     {
-        return $"Фраза на кириллице: {{CyrillicPhrase}}\\nФраза на латинице: {{LatinPhrase}}\\nСгенерированный пароль: {{GeneratedPassword}}";
+        return $"Фраза на кириллице: {CyrillicPhrase}\nФраза на латинице: {LatinPhrase}\nСгенерированный пароль: {GeneratedPassword}";
     }
 }
 
 class GeneratePassword
 {
+    private Random random;
+    
     public InformationAboutPassword InformationAboutPassword()
     {
-        string verbsFilePath = @"C:\Users\litpu\RiderProjects\Ex2\Verbs.txt";
-        string nounsFilePath = @"C:\Users\litpu\RiderProjects\Ex2\Nouns.txt";
+        string verbsFilePath = @"C:\Users\litpu\RiderProjects\passwordClass\Verbs.txt";
+        string nounsFilePath = @"C:\Users\litpu\RiderProjects\passwordClass\Nouns.txt";
 
         string[] verbs = File.ReadAllLines(verbsFilePath);
         string[] nouns = File.ReadAllLines(nounsFilePath);
@@ -35,15 +37,42 @@ class GeneratePassword
 
         string verb = GetRandomElement(verbs);
         string noun = GetRandomElement(nouns);
+        string noun_2 = GetRandomElement(nouns);
 
-        string cyrillicPassword = $"{nouns} {verbs} {nouns}";
-        return new InformationAboutPassword(cyrillicPassword, );
+        string cyrillicPassword = $"{noun} {verb} {noun_2}";
+        string latinPassword = ConvertToEnglishLayout(cyrillicPassword);
+        string generatedPassword = Generation(new string[] { latinPassword });
+        return new InformationAboutPassword(cyrillicPassword, latinPassword, generatedPassword);
     }
 
     private string GetRandomElement(string[] array)
     {
-        Random random = new Random();
+        random = new Random();
         return array[random.Next(array.Length)];
+    }
+
+    private string ConvertToEnglishLayout(string cyrillicPassword)
+    {
+        Dictionary<char, char> layoutMapping = new Dictionary<char, char>
+        {
+            { 'а', 'f' }, { 'б', ',' }, { 'в', 'd' }, { 'г', 'u' }, { 'д', 'l' },
+            { 'е', 't' }, { 'ё', '`' }, { 'ж', ';' }, { 'з', 'p' }, { 'и', 'b' },
+            { 'й', 'q' }, { 'к', 'r' }, { 'л', 'k' }, { 'м', 'v' }, { 'н', 'y' },
+            { 'о', 'j' }, { 'п', 'g' }, { 'р', 'h' }, { 'с', 'c' }, { 'т', 'n' },
+            { 'у', 'e' }, { 'ф', 'a' }, { 'х', '[' }, { 'ц', 'w' }, { 'ч', 'x' },
+            { 'ш', 'i' }, { 'щ', 'o' }, { 'ъ', 'm' }, { 'ы', 's' }, { 'ь', '\'' },
+            { 'э', ']' }, { 'ю', '.' }, { 'я', 'z' }, { ' ', ' ' }
+        };
+
+        return new string(cyrillicPassword.ToLower().ToCharArray().Select(c =>
+            layoutMapping.ContainsKey(c) ? layoutMapping[c] : c).ToArray());
+    }
+
+    private string Generation(string[] latinPassword)
+    {
+        string[] selectedWords = latinPassword.OrderBy(w => random.Next()).Take(random.Next(3, 5)).ToArray();
+        string generatedPassword = string.Join("", selectedWords.Select(w => w.Substring(0, Math.Min(3, w.Length))));
+        return generatedPassword;
     }
 }
 
@@ -53,5 +82,7 @@ class Program
     {
         GeneratePassword generatePassword = new GeneratePassword();
         InformationAboutPassword informationAboutPassword = generatePassword.InformationAboutPassword();
+        Console.WriteLine(informationAboutPassword);
+        
     }
 }
