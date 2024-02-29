9910190b39943134dca45c20f78a449cd626bb43
litpu <litput0@gmail.com>
2024-02-29 15:50:38 +0300

    Encapsulation

--- AddCode.Studying.Services/PasswordInfo.cs
+++ AddCode.Studying.Services/PasswordInfo.cs
@@ -4,12 +4,16 @@ namespace AddCode.Studying.Services;
 
 public class PasswordInfo(string cyrillicPhrase, string latinPhrase, string generatedPassword)
 {
+    private string CyrillicPhrase { get; } = cyrillicPhrase;
+    private string LatinPhrase { get; } = latinPhrase;
+    private string GeneratedPassword { get; } = generatedPassword;
+
     public override string ToString()
     {
         var stringBuilder = new StringBuilder();
-        stringBuilder.AppendLine($"Фраза на кириллице: {cyrillicPhrase}");
-        stringBuilder.AppendLine($"Фраза на латинице: {latinPhrase}");
-        stringBuilder.AppendLine($"Сгенерированный пароль: {generatedPassword}");
+        stringBuilder.AppendLine($"Фраза на кириллице: {CyrillicPhrase}");
+        stringBuilder.AppendLine($"Фраза на латинице: {LatinPhrase}");
+        stringBuilder.AppendLine($"Сгенерированный пароль: {GeneratedPassword}");
 
         return stringBuilder.ToString();
     }
